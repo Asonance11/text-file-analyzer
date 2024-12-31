8e52dc2134fb5a55b98f635a8e0114f64b5e11b7
Oluwadabira <adigunsamuelaso@gmail.com>
2024-12-31 15:28:55 +0100

    Add line count

--- main.c
+++ main.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 
-int character_count(FILE *file){
-	int count = 0;
-	char ch;
+int character_count(FILE *file) {
+  int count = 0;
+  char ch;
 
-	while((ch = fgetc(file)) != EOF){
-		if(ch == ' ' || ch == '\n'){
-			continue;
-		}else{
-			count++;
-		}
-	}
+  while ((ch = fgetc(file)) != EOF) {
+    if (ch == ' ' || ch == '\n') {
+      continue;
+    } else {
+      count++;
+    }
+  }
 
-	return count;
+  return count;
 }
 
-int main(int argc, char *argv[])
-{
-	FILE *file;
+int line_count(FILE *file) {
+  int lines = 0;
+  char ch;
 
-	file = fopen("example.txt", "r");
+  while ((ch = fgetc(file)) != EOF) {
+    if (ch == '\n') {
+      lines++;
+    }
+  }
 
-	if(file == NULL){
-		printf("Unable to open file \n");
-		return 0;
-	}
+  return lines;
+}
 
-	printf("The number of characters in the file is %d\n", character_count(file));
+int main(int argc, char *argv[]) {
+  FILE *file;
 
-	fclose(file);
+  file = fopen("example.txt", "r");
 
-	return 0;
+  if (file == NULL) {
+    printf("Unable to open file \n");
+    return 0;
+  }
+
+  printf("The number of characters in the file is %d\n", character_count(file));
+  rewind(file);
+  printf("The number of lines in the file is %d\n", line_count(file));
+
+  fclose(file);
+
+  return 0;
 }
